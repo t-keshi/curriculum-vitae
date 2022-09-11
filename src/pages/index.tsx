8b3e081c5670924b00223406f1e61dfe22edbd2c
t-keshi <quick.resp.biz094@gmail.com>
2022-09-11 11:57:01 +0900

    feature: sort by graphql

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,7 +7,7 @@ import { SEO } from '../components/ui/SEO';
 
 export const pageQuery = graphql`
   query Resume {
-    allMdx {
+    allMdx(sort: { fields: slug }, skip: 1) {
       nodes {
         body
         slug
@@ -17,7 +17,7 @@ export const pageQuery = graphql`
 `;
 
 const About: React.FC<PageProps<GatsbyTypes.ResumeQuery>> = ({ data }) => {
-  if (!data || !data.allMdx?.nodes[0].body || !data.allMdx?.nodes[0].slug) {
+  if (!data || !data.allMdx.nodes[0].body || !data.allMdx.nodes[0].slug) {
     return null;
   }
 
@@ -26,13 +26,9 @@ const About: React.FC<PageProps<GatsbyTypes.ResumeQuery>> = ({ data }) => {
       <SEO title="職務経歴書" />
       <Layout pageTitle="職務経歴書">
         <Box mb={12} />
-        {data.allMdx.nodes
-          .slice(0)
-          .sort((node) => parseInt(node.slug?.slice(0, 2) ?? '1', 10))
-          .slice(1)
-          .map((node) => (
-            <MDXRenderer key={node.slug}>{node.body}</MDXRenderer>
-          ))}
+        {data.allMdx.nodes.map((node) => (
+          <MDXRenderer key={node.slug}>{node.body}</MDXRenderer>
+        ))}
         <Box mb={24} />
         {/* <LinkButton
           as="button"
